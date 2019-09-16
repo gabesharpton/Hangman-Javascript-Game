708adcc9024e2d98730b5b6cb836c86a06e98801
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-09-16 14:37:59 -0500

    created buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let randomWord = "";
+let guessAmount = 6;
+
+
+/*
+            --------------------Basics of what I need------------------
+A basic function to guess the word you input first.
+You get 6 wrong guesses
+A key board with each letter of the alphabet
+A guess line with underscores for how many letters the hidden word has
+The letter dissapears after selected
+If the letter is correct display on guess line, if not it will take away a guess and display body part
+
+
+
+            --------------------Extra I would like------------------
+Random letter bank
+has random button option instead of word you choose
+Maybe add pictures instead of letter for body parts
+
+
+*/

58d7bea12eb09e8cafecdc59d71e68917ba9df4d
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-09-17 17:02:33 -0500

    got first part working with alerts and prompts

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let modal = document.getElementById('modal');
 let close = document.getElementById('close');
 let rules = document.getElementById('rules');
 let word = document.getElementById('input');
-let submit = document.getElementById('submit')
-
+let submit = document.getElementById('submit');
+let random = document.getElementById('random');
 let newWord = document.getElementById('guessingWord');
 
 function dropBox() {
@@ -17,38 +17,69 @@ function ruleOpen() {
     modal.style.display = 'block';
 
 }
-function wordSelect() {
-    let word = input.value;
-    console.log(input.value);
+// function wordSelect() {
+//     let word = input.value;
+//     console.log(input.value);
 
-    // newWording.appendChild(word);
+//     // newWording.appendChild(word);
     
-    // document.body.appendChild(newWord);
-}
+//     // document.body.appendChild(newWord);
+// }
+//random.addEventListener('click', () => {
 
-let guessingWord = words[Math.floor(Math.random()* words.length)];
-for(let i = 0; i < guessingWord.length; i++){
-    guessWord[i] = "_";
-}
-let remainingLetters = guessingWord.length;
+    let guessingWord = words[Math.floor(Math.random()* words.length)];
+    for(let i = 0; i < guessingWord.length; i++){
+        guessWord[i] = "_";
+    }
+//})
 
+var remainingLetters = guessingWord.length;
 //////////////////------------Main game loop-------------
 
-// while(remainingLetters > 0){
-    //     guessWord.join(" ");
+while(remainingLetters > 0){
+        alert(guessWord.join(" "));
+    var guess = prompt('guess a letter');
+        ///if cancle is selected
+        if(guess == null){
+                //end loop
+            break;
+            //if guess is more than one letter
+        }else if (guess.length !== 1){
+            alert('use single letter');
+            //valid guess
+        }else{
+            //update the game state with the guess
+            for(var j = 0; j < guessingWord.length; j++){
+                //if the letter the guess is in the word
+
+                if(guessingWord[j] === guess){
+                    //update answer array
+                    guessWord[j] = guess;
+                    //subtract one from remaining
+                    remainingLetters--;
+                }
+            }
+        }
+        ///////---------end game loop---------////
+}
+
+
     
-    // }
+
+
+
     ///Had help from carlos for this.
-    submit.addEventListener('click', () => {
-        console.log(word.value);
-        let newDiv = document.createElement('div')
-        let newContent = document.createTextNode(`${input.value}`)
-    newDiv.appendChild(newContent)
-    newWord.appendChild(newDiv)
-  })
+//     submit.addEventListener('click', () => {
+//         console.log(word.value);
+//         let newDiv = document.createElement('div')
+//         let newContent = document.createTextNode(`${input.value}`)
+//     newDiv.appendChild(newContent)
+//     newWord.appendChild(newDiv)
+//   })
 
-
-
+//console.log(newWord)
+//console.log(newContent)
+//console.log(submit)
 
 rules.addEventListener('click', ruleOpen)
 close.addEventListener('click', dropBox);
